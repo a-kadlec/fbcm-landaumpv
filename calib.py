ebb11e0e826a9561d4a1eff13251a20140737ad3
Á. Kadlecsik <50501820+a-kadlec@users.noreply.github.com>
2024-10-08 00:18:59 +0200

    Refined fit
    Code now creates on refined fit (landau fit, seeded with an initial landau fit, and omitting outlier points due to known inhomogenity in the detector)
    Now also seamlessly usable for boards with 2 asics

--- calib.py
+++ calib.py
@@ -57,18 +57,9 @@ def process_npz(npz):
 
 
 
-def get_board_calibration(config_file):
+def get_ASIC_calibration(data, sample, config_file):
     matplotlib.rc('font', size=16)
     
-    
-
-    samplenum = config_file['board_number']
-    side = "_"+config_file['measurement']
-    sample = str(samplenum) + side
-    file_num = 14
-
-    data = process_npz(dict(np.load(config_file['calibration_data'], allow_pickle=True, encoding="ASCII")))
-
 
     results = data['toa_tot']['results']
     test_conditions = data['toa_tot']['test_conditions']
@@ -83,6 +74,7 @@ def get_board_calibration(config_file):
 
     step = test_conditions['step']
 
+    print("Calibration fitting for "+sample)
     print("Tested thresholds: ",test_conditions['th_list_fC'])
     print("Tested RCs: ",test_conditions['rccal'])
 
@@ -99,8 +91,13 @@ def get_board_calibration(config_file):
 
         toa =     results['toa'][ch_num].copy()
         tot =     results['tot'][ch_num].copy()
-        tot_std = results['tot_std'][ch_num].copy()
 
+        bUseSigma = False
+        try:
+            tot_std = results['tot_std'][ch_num].copy()
+            bUseSigma = True
+        except:
+            print("Warning: no tot_std key found in results! You are running on old data! (And fits may fail)")
 
         tw = twalk[ch_num].copy()
         # print(tw.shape)
@@ -130,10 +127,12 @@ def get_board_calibration(config_file):
                 # start = np.argmin(err[r,0:256:step])*step # find first False what means first valid value
                 if 'start' in test_conditions:
                     start = test_conditions['start']
-                    if not isinstance(start, int):
+                    if not isinstance(start, int) and not isinstance(start, np.int64):  
                         start = int(test_conditions['start'][ch_num,r,th_level_fC])
                 elif 'start' in results:
-                    start = int(results['start'][ch_num,r,th_level_fC])
+                    start = results['start']
+                    if not isinstance(start, int) and not isinstance(start, np.int64):
+                        start = int(results['start'][ch_num,r,th_level_fC])
                 else:
                     start = 0
                 end = 255
@@ -148,8 +147,13 @@ def get_board_calibration(config_file):
                 mychcalval = np.concatenate(([th_level_fC_val*1e-15],chcalval[start:end:step]))
 
 
+                if bUseSigma:
+                    sigma_fit = tot_std[r,th_level_fC,start:end:step]*1e9
+                else:
+                    sigma_fit = np.array( [1] * len(myTot) )
+
                 #pfit = curve_fit(tot_func, mychcalval*1e15, mytot_fit*1e9, p0=(11,2,0.01,1), sigma = mytot_sigma*1e9, absolute_sigma = True)
-                popt, pcov = curve_fit(tot_func, chcalval[start:end:step]*1e15, myTot*1e9, p0=config_file['log_p0'], sigma = tot_std[r,th_level_fC,start:end:step]*1e9, absolute_sigma = True)
+                popt, pcov = curve_fit(tot_func, chcalval[start:end:step]*1e15, myTot*1e9, p0=config_file['log_p0'], sigma = sigma_fit, absolute_sigma = True)
                 # , p0=(11,2,0.01,1)
 
                 # pfit = np.polyfit(mychcalval*1e15, myTot*1e9, 3)
@@ -206,7 +210,8 @@ def get_board_calibration(config_file):
                 #print(chcalval[start]*1e15)
                 #ax.plot(chcalval[start:end:step]*1e15, toa[r,start:end:step]*1e9, 'sr', label='ToA')
                 ax.plot(chcalval[start:end:step]*1e15, tot[r,th_level_fC,start:end:step]*1e9, 'sb', label='ToT')
-                ax.errorbar(chcalval[start:end:step]*1e15, tot[r,th_level_fC,start:end:step]*1e9, yerr=tot_std[r,th_level_fC,start:end:step]*1e9, capsize=3, fmt=".", ecolor = "black")
+                if bUseSigma:
+                    ax.errorbar(chcalval[start:end:step]*1e15, tot[r,th_level_fC,start:end:step]*1e9, yerr=tot_std[r,th_level_fC,start:end:step]*1e9, capsize=3, fmt=".", ecolor = "black")
                 ax.plot(chcalval[start:end:step]*1e15, tw[r,th_level_fC,start:end:step]*1e9, 'or', label='TW')
 
                 goodlim_x = ax.get_xlim()
@@ -227,8 +232,49 @@ def get_board_calibration(config_file):
                 #fig.legend(loc=[0.14,0.82], prop={'size': 10})
                 ax.legend(loc='best', prop={'size': 10})  # bbox_to_anchor=(0, 0, 0.9, 0.9)
                 # plt.yscale('log')
-                fig.savefig(config_file['output_dir']+'/calib_plots/tot_tw_sample_%s_%s_channel_%d_rccal_%d_th_%s.jpg' % (sample, file_num, ch_num, test_conditions['rccal'][r],  test_conditions['th_list_fC'][th_level_fC]), format='jpg', bbox_inches='tight')
+                fig.savefig(config_file['output_dir']+'/calib_plots/tot_tw_sample_%s_channel_%d_rccal_%d_th_%s.jpg' % (sample, ch_num, test_conditions['rccal'][r],  test_conditions['th_list_fC'][th_level_fC]), format='jpg', bbox_inches='tight')
                 plt.close()
+    return output
+
+
+
+
+def get_board_calibration(config_file):
+    matplotlib.rc('font', size=16)
+    
+    
+    samplenum = config_file['board_number']
+    meas = "_"+config_file['measurement']
+    sample = str(samplenum) + meas
+
+    if config_file['calibration_data_left']:
+        sample = sample + "_R"
+
+    data = process_npz(dict(np.load(config_file['calibration_data'], allow_pickle=True, encoding="ASCII")))
+
+    output = get_ASIC_calibration(data, sample, config_file)
+
+    if config_file['calibration_data_left']:
+        data_left = process_npz(dict(np.load(config_file['calibration_data_left'], allow_pickle=True, encoding="ASCII")))
+        sample_left = str(samplenum) + meas + "_L"
+        output_left = get_ASIC_calibration(data_left,sample_left, config_file)
+
+        # ASICS use channels 0,2,5, Right:0,1,2, Left:3,4,5
+        for i in range(len(output['channel'])):
+            if output['channel'][i] == 2:
+                output['channel'][i] = 1
+            elif output['channel'][i] == 5:
+                output['channel'][i] = 2
+        for i in range(len(output_left['channel'])):
+            if output_left['channel'][i] == 0:
+                output_left['channel'][i] = 3
+            elif output_left['channel'][i] == 2:
+                output_left['channel'][i] = 4
+
+        for key in output.keys():
+            output[key].extend(output_left[key])
+
+    #print(output)
     return output
 
 
--- landaumvp.py
+++ landaumvp.py
@@ -36,7 +36,6 @@ def main(argv: list[str] = sys.argv[1:]):
     if args.suppress_warnings:
         calib.hide_warnings(True)
 
-    print("Calibration fitting...")
     calibration = calib.get_board_calibration(config_file)
 
 
@@ -48,13 +47,15 @@ def main(argv: list[str] = sys.argv[1:]):
 
     output_dir = Path(config_file['output_dir'])
     print("Raw ToT plot...")
-    raw_tot_plot(histograms_data, output_dir/f"tot_raw.png", config_file)
-    print("Charge plot [log calib]...")
-    landaumvp_fit(histograms_data, output_dir/f"tot_fc_fit_log-cal.png", config_file, calibration, "log")
-    print("Charge plot [spline calib]...")
-    landaumvp_fit(histograms_data, output_dir/f"tot_fc_fit_spline-cal.png", config_file, calibration, "spline")
-    print("Common Charge plot [log vs pline calib]...")
-    landaumvp_fit(histograms_data, output_dir/f"tot_fc_fit.png", config_file, calibration, "both")
+    raw_tot_plot(histograms_data, output_dir/f"tot_raw_board{config_file['board_number']}_{config_file['measurement']}_rc{config_file['RC']}.png", config_file)
+    #print("Charge plot [log calib]...")
+    #landaumvp_fit(histograms_data, output_dir/f"tot_fc_fit_log-cal.png", config_file, calibration, "log")
+    #print("Charge plot [spline calib]...")
+    #landaumvp_fit(histograms_data, output_dir/f"tot_fc_fit_spline-cal.png", config_file, calibration, "spline")
+    #print("Common Charge plot [log vs pline calib]...")
+    #landaumvp_fit(histograms_data, output_dir/f"tot_fc_fit.png", config_file, calibration, "both")
+    print("Refined Landau plot...")
+    landaumvp_refined_fit(histograms_data, output_dir/f"tot_fc_fit-refined_board{config_file['board_number']}_{config_file['measurement']}_rc{config_file['RC']}.png", config_file, calibration, "log")
 
 
 def get_histograms(data: bytes, timestamps: list = []) -> dict[str, list]:
@@ -110,6 +111,9 @@ def _get_intra_and_interspill_hists(timestamps, hist_data):
     return sum(hist_data[2::2]), sum(scaled_noise)
 
 
+def _mpv_to_thickness(mpv):
+    return mpv * 1e-15 / 1.6e-19 / 78
+
 def gauss(x, H, A, x0, sigma): 
     return H + A * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
 
@@ -149,7 +153,13 @@ def raw_tot_plot(histograms_data, output_file, config_file):
                     y_axis = y_axis[:i]
                     break
     
-        ax.plot(time_bins, y_axis, ".", color="black")   
+        errorbars = np.sqrt(y_axis)
+        #print(y_axis)
+        #print(errorbars)
+
+        #ax.plot(time_bins, y_axis, ".", color="black")   
+        ax.plot(time_bins, y_axis, 'sb')
+        ax.errorbar(time_bins, y_axis, yerr=errorbars, capsize=3, fmt=".", ecolor = "black")
 
         fig.suptitle("ToT distribution of board25 RC"+str(config_file['RC']))
         ax.set_title(f"Channel {channel_number}, th: {config_file['th'][channel_number]} fC")
@@ -221,7 +231,7 @@ def landaumvp_fit(histograms_data, output_file, config_file, calibration, calib_
                     y_axis2[i:]
                     break
             for i in range(len(fc_bins2)):
-                if fc_bins2[i] > 15:
+                if fc_bins2[i] > config_file['cutoff_fC']:
                     fc_bins2 = fc_bins2[:i]
                     y_axis2 = y_axis2[:i]
                     break    
@@ -233,13 +243,14 @@ def landaumvp_fit(histograms_data, output_file, config_file, calibration, calib_
                 break
 
         for i in range(len(fc_bins)):
-            if fc_bins[i] > 15:
+            if fc_bins[i] > config_file['cutoff_fC']:
                 fc_bins = fc_bins[:i]
                 y_axis = y_axis[:i]
                 break
         
 
-        ax.plot(fc_bins, y_axis, ".", color="black")    
+        #ax.plot(fc_bins, y_axis, ".", color="black")    
+        ax.errorbar(fc_bins, y_axis, yerr=np.sqrt(y_axis), capsize=3, fmt=".", ecolor = "black")
 
         popt, pcov = curve_fit(lambda x, N, mpv, xi: N*landau.pdf(x, mpv, xi), fc_bins, y_axis, p0 = config_file['landau_p0'])
         perr = np.sqrt(np.diag(pcov))
@@ -254,15 +265,16 @@ def landaumvp_fit(histograms_data, output_file, config_file, calibration, calib_
             perr_landau_2 = np.sqrt(np.diag(pcov_landau_2))
 
 
+
         x_axis = np.linspace(math.floor(min(fc_bins)), max(fc_bins), 1000)
 
         if calib_type != "both":
-            ax.plot(x_axis, popt2[0]*langauss.pdf(x_axis, popt2[1], popt2[2], popt2[3]), "-", color="red", label = "LandauXGauss MPV=%.2f±%.2f" % (popt2[1],perr2[1]))    
-            ax.plot(x_axis, popt[0]*landau.pdf(x_axis, popt[1], popt[2]), "--", color="blue", label = "Landau MPV=%.2f±%.2f" % (popt[1],perr[1]))    
+            ax.plot(x_axis, popt2[0]*langauss.pdf(x_axis, popt2[1], popt2[2], popt2[3]), "-", color="red", label = "LandauXGauss MPV=(%.2f±%.2f) fC" % (popt2[1],perr2[1]))    
+            ax.plot(x_axis, popt[0]*landau.pdf(x_axis, popt[1], popt[2]), "--", color="blue", label = "Landau MPV=(%.2f±%.2f) fC" % (popt[1],perr[1]))    
         else:
-            ax.plot(x_axis, popt[0]*landau.pdf(x_axis, popt[1], popt[2]), "-", color="red", label = "[log-cal] MPV=%.2f±%.2f" % (popt[1],perr[1]))   
+            ax.plot(x_axis, popt[0]*landau.pdf(x_axis, popt[1], popt[2]), "-", color="red", label = "[log-cal] MPV=(%.2f±%.2f) fC" % (popt[1],perr[1]))   
             x_axis2 = np.linspace(math.floor(min(fc_bins2)), max(fc_bins2), 1000)
-            ax.plot(x_axis2, popt_landau_2[0]*landau.pdf(x_axis2, popt_landau_2[1], popt_landau_2[2]), linestyle=(0, (5, 10)), color="blue", label = "[spline-cal] MPV=%.2f±%.2f" % (popt_landau_2[1],perr_landau_2[1]))   
+            ax.plot(x_axis2, popt_landau_2[0]*landau.pdf(x_axis2, popt_landau_2[1], popt_landau_2[2]), linestyle=(0, (5, 10)), color="blue", label = "[spline-cal] MPV=(%.2f±%.2f) fC" % (popt_landau_2[1],perr_landau_2[1]))   
 
 
         ax.legend(loc='best', prop={'size': 11})
@@ -274,11 +286,126 @@ def landaumvp_fit(histograms_data, output_file, config_file, calibration, calib_
         ax.set_title(f"Channel {channel_number}, th: {calibration['th'][calib_i]} fC")
         ax.set_xticks(np.arange(math.floor(min(fc_bins)), math.ceil(max(fc_bins)), 2.0))
         ax.set_xticks(np.arange(math.floor(min(fc_bins)), math.ceil(max(fc_bins)), 1.0), minor=True)
-        ax.set_ylabel(f"Count / {bin_width:.2f} ns")
+        ax.set_ylabel(f"Count")
         ax.set_xlabel("Charge [fC]")
         ax.grid(linestyle = "--")
     fig.savefig(output_file)
     plt.close(fig)
+
+
+
+
+
+
+def landaumvp_refined_fit(histograms_data, output_file, config_file, calibration, calib_type = "log"):
+
+    cutoff = config_file['cutoff']
+    axs = []
+    bottom_axs = []
+    fig = plt.figure(tight_layout=True, figsize=(15, 10))
+    gs_top_level = plt.GridSpec(2, 3, figure=fig)
+    for i in range(6):
+        gs = gs_top_level[i].subgridspec(2, 1, height_ratios=[4, 1], hspace=0)
+        axs.append(fig.add_subplot(gs[0]))
+        bottom_axs.append(0)
+
+    for (hist_name, (timestamps, hist_data)), (ax, bottom_ax) in zip(histograms_data.items(), zip(axs, bottom_axs)):
+        timestamps, hist_data = _clean_histograms_arrays(timestamps, hist_data)
+
+        channel_number = int(hist_name[-1])
+
+        intraspill_sum, interspill_sum = _get_intra_and_interspill_hists(timestamps, hist_data)
+        time_bins = np.arange(len(interspill_sum))/1.28
+        bin_width = time_bins[-1]/len(time_bins)
+        time_bins += bin_width/2
+
+
+        y_axis = interspill_sum - intraspill_sum  # was intra - inter, reversed
+
+        if cutoff:
+            for i in range(len(time_bins)):
+                if time_bins[i] > cutoff:
+                    time_bins = time_bins[:i]
+                    y_axis[:i]
+                    break
+    
+
+        # Conversion of x axis to fc
+        # find calib for this setup
+        for calib_i in range(len(calibration['RC'])): 
+            if calibration['th'][calib_i] == config_file['th'][channel_number]  and calibration['RC'][calib_i] == config_file['RC'] and calibration['channel'][calib_i] == channel_number:
+                break
+
+        if calib_type == "log":
+            fc_bins = calib.invert_tot_func(time_bins, *calibration['fitparams'][calib_i])
+        if calib_type == "spline":
+            fc_bins = BSpline(*calibration['spline'][calib_i])(time_bins)
+
+        for i in range(len(fc_bins)):
+            if fc_bins[i] > 0:  # to remove nans
+                fc_bins = fc_bins[i:]
+                y_axis[i:]
+                break
+
+        for i in range(len(fc_bins)):
+            if fc_bins[i] > config_file['cutoff_fC']:
+                fc_bins = fc_bins[:i]
+                y_axis = y_axis[:i]
+                break
+        
+
+        #ax.plot(fc_bins, y_axis, 'sb') 
+        #ax.errorbar(fc_bins, y_axis, yerr=np.sqrt(y_axis), capsize=3, fmt=".", ecolor = "black")
+
+        popt, pcov = curve_fit(lambda x, N, mpv, xi: N*landau.pdf(x, mpv, xi), fc_bins, y_axis, p0 = config_file['landau_p0'])
+
+        # refine fit:
+        fc_bins_fit = []
+        y_axis_fit = []
+        fc_bins_off = []
+        y_axis_off = []
+        for i in range(len(y_axis)):
+            if (fc_bins[i] < popt[1] or abs(y_axis[i] -  popt[0]*landau.pdf(fc_bins[i], popt[1], popt[2])) / y_axis[i] < config_file['cutoff_ref']):    # and y_axis[i] > 0 
+                fc_bins_fit.append(fc_bins[i])
+                y_axis_fit.append(y_axis[i])
+            else:
+                fc_bins_off.append(fc_bins[i])
+                y_axis_off.append(y_axis[i])
+
+
+        ax.plot(fc_bins_fit, y_axis_fit, 'sb') 
+        ax.errorbar(fc_bins_fit, y_axis_fit, yerr=np.sqrt(y_axis_fit), capsize=3, fmt=".", ecolor = "black")
+        ax.plot(fc_bins_off, y_axis_off, '.', color="black") 
+        ax.errorbar(fc_bins_off, y_axis_off, yerr=np.sqrt(y_axis_off), capsize=3, fmt="x", ecolor = "black", color="black")
+
+        popt_ref, pcov_ref = curve_fit(lambda x, N, mpv, xi: N*landau.pdf(x, mpv, xi), fc_bins_fit, y_axis_fit, p0 = popt) #, sigma = np.sqrt(y_axis_fit), absolute_sigma = True)
+        perr_ref = np.sqrt(np.diag(pcov_ref))
+
+        x_axis_fit = np.linspace(min(fc_bins_fit) if min(fc_bins_fit) > math.floor(min(fc_bins_off)) else math.floor(min(fc_bins_fit)), max(fc_bins_fit), 1000)
+        x_axis_off = np.linspace(math.floor(min(fc_bins_off)) if math.floor(min(fc_bins_off)) < max(fc_bins_fit) else max(fc_bins_fit), max(fc_bins_off), 1000)
+
+        mpv_string = eval( "'%."+str(config_file['mpv_display_precision'])+"f±%."+str(config_file['mpv_display_precision'])+"f' % (popt_ref[1],perr_ref[1])" )
+        ax.plot(x_axis_off, popt_ref[0]*landau.pdf(x_axis_off, popt_ref[1], popt_ref[2]), "--", color="crimson")  
+        ax.plot(x_axis_fit, popt_ref[0]*landau.pdf(x_axis_fit, popt_ref[1], popt_ref[2]), "-", color="crimson", label = f"MPV=({mpv_string}) fC")  
+                # "MPV=(%.2f±%.2f) fC" % (popt_ref[1],perr_ref[1]))  
+
+
+        ax.legend(loc='best', prop={'size': 11})
+        fig.suptitle(f"Landau Distribution fits to Amplitude Spectrum of board25, RC{config_file['RC']}, {calib_type}-cal.")
+
+        ax.set_title(f"Channel {channel_number}, th: {calibration['th'][calib_i]} fC")
+        ax.set_xticks(np.arange(math.floor(min(fc_bins)), math.ceil(max(fc_bins)), 2.0))
+        ax.set_xticks(np.arange(math.floor(min(fc_bins)), math.ceil(max(fc_bins)), 1.0), minor=True)
+        ax.set_ylabel(f"Count")
+        ax.set_xlabel("Charge [fC]")
+        ax.grid(linestyle = "--")
+
+        print("Channel "+str(channel_number)+" eff. thickness: (%.2f±%.2f) um" % (_mpv_to_thickness(popt_ref[1]) , _mpv_to_thickness(perr_ref[1])))
+    fig.savefig(output_file)
+    plt.close(fig)
+
+
+
 
 
 if __name__ == "__main__":
